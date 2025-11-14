b125dc8d3e808cc939f0b4be774868c770f62de4
Abdullah albagshi <abdullah.albagshi@gmail.com>
2025-11-14 22:45:39 +0300

    Add personal introduction and contact links to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@
 export default function Home() {
 	return (
 		<div className='max-w-screen-xl mx-auto font-[family-name:var(--font-inter)]'>
+      <p>Hi I'm Abdullah Albagshi</p>
+      <p>I'm a software engineer</p>
+      <a href="https://github.com/abdullah-albagshi" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://www.linkedin.com/in/abdullah-albagshi/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="mailto:abdullah.albagshi@gmail.com">Email</a>
 		</div>
 	);
 }
